57a700a5f6276fa65d25fb5ccb36f9cb57e45c2a
nkantor <nick.kantor@utexas.edu>
2016-06-07 20:09:21 -0600

    Finished with the scraper

--- Makefile
+++ Makefile
@@ -2,13 +2,29 @@
 .PHONY: all
 all:
 	rm -rf pcap_urls_panda
-	# rm -rf pcap_urls_mta
 	python panda.py > pcap_urls_panda
-	# python mta.py > pcap_urls_mta
 	sh transfer.sh pcap_urls_panda
-	# sh transfer.sh pcap_urls_mta
+	
+	rm -rf pcap_urls_mta
+	python mta.py > pcap_urls_mta
+	sh transfer.sh pcap_urls_mta
+
+panda:
+	rm -rf pcap_urls_panda
+	python panda.py > pcap_urls_panda
+	sh transfer.sh pcap_urls_panda
+
+mta:
+	rm -rf pcap_urls_mta
+	python mta.py > pcap_urls_mta
+	sh transfer.sh pcap_urls_mta
 
 # Transfers the files
 .PHONY: transfer
 transfer:
-	sh transfer.sh pcap_urls
+	sh transfer.sh pcap_urls_panda
+	sh transfer.sh pcap_urls_mta
+
+clean:
+	rm -rf pcap_urls_panda
+	rm -rf pcap_urls_mta
--- transfer.sh
+++ transfer.sh
@@ -3,17 +3,22 @@
 # specify the path here
 PATH="/home/nkantor/pcaps/"
 
+# commands
 WGET=/usr/bin/wget 
 ECHO=/bin/echo
 SHA1=/usr/bin/sha1sum
 AWK=/usr/bin/awk
 
-for filename in *; do
-	$CURL $filename $PATH
-done
-
+# loop through every url
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    # echo "Text read from file: $line"
-    SHA_LINE = `$ECHO -n $line | $SHA1 | $AWK '{print $1}'` 
-    $WGET -O ${PATH}${SHA_LINE}.pcap $line
+    # create a SHA1 hash of the url
+    F=`$ECHO -n $line | $SHA1 | $AWK '{print $1}'`
+
+    # check if the file exists. If it does then do nothing, otherwise download it with wget and save it.
+    if [ -f ${PATH}${F}.pcap ]
+    then
+    	echo "${PATH}${F}.pcap already downloaded"
+    else
+    	$WGET -O ${PATH}${F}.pcap $line
+    fi
 done < "$1"
